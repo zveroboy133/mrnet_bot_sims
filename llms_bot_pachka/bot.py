48396948a7c511cba3214ee5c3afa9d26628ea20
vitaly <zveroboy133@gmail.com>
2025-06-24 17:40:41 +0300

    # тест обновленной ветки

--- llms_bot_pachka/bot.py
+++ llms_bot_pachka/bot.py
@@ -7,6 +7,7 @@ from typing import Dict, Any
 from datetime import datetime
 from flask import Flask, request, jsonify
 
+# тест обновленной ветки
 # Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
